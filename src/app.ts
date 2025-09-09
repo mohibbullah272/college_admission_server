367cca911e826c1bc2e14b2fb8c465602454fcca
mohibbullah272 <bullahmohi50@gmail.com>
2025-09-09 13:42:41 +0600

    add initial setup

---
+++ src/app.ts
@@ -0,0 +1,30 @@
+import { Application, Request, Response } from "express";
+import express from 'express';
+import cors from 'cors'
+
+
+
+const app:Application = express()
+
+app.use(cors())
+app.use(express.json())
+
+
+
+app.get("/healthz", (req:Request,res:Response) => {
+    res.send("ok");
+  });
+  
+app.get('/',async(req:Request,res:Response)=>{
+    res.send(`welcome to college admission portal server`)
+})
+
+
+
+
+
+
+export default app
+
+
+
